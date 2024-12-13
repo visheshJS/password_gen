bdc7f1e70963dca35a126edf0253c614bbad4df4
visheshJS <visheshsharma_23me287@dtu.ac.in>
2024-12-14 02:04:54 +0530

    digit selection feature added

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ const allCharacters = [
     // Numbers
     '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
     // Symbols 
-    '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+    '!', '@', '#', '$', '%', '&',
     // Uppercase Letters (A-Z)
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
     // Lowercase Letters (a-z)
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     // Other Symbols from Keyboard
-    '`', '~', '-', '_', '=', '+', '[', '{', ']', '}', '|', ';', ':', ',', '<', '.', '>', '/', '?'
+    '_', '?'
 ];
 
 const copyImg = document.getElementById("copy-btn");
@@ -28,14 +28,17 @@ const passContainer = document.querySelector(".password-cont");
 
 
 
-function Generate_12(){
+
+function Generate(){
+    const lengthSelector =document.getElementById("password-length");
+    const selectedLength = parseInt(lengthSelector.value);
     const passElements = passContainer.querySelectorAll("p");
     passElements.forEach(element => element.remove());
     
 
     let pass ="";
 
-    for(let i =1; i<=12;i++){
+    for(let i =1; i<=selectedLength;i++){
         let random_index = Math.floor(Math.random()*allCharacters.length);
         pass+= allCharacters[random_index];
 
